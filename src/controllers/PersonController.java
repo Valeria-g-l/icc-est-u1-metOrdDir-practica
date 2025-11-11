4b4f8373c80f5c5b75bb0136b902a0c917e73414
Valeria-g-l <valeriaguamanleon03.com>
2025-11-11 13:48:50 +0000

    Practica Metodo ordenamiento

--- src/controllers/PersonController.java
+++ src/controllers/PersonController.java
@@ -1,24 +1,27 @@
 package controllers;
+
 import models.Person;
 
 public class PersonController {
-    public PersonController(){
+    public PersonController() {
 
     }
 
-    public void sortByName(Person[] people) {  //Metodo bubble sort para NOMBRES
+    // Metodo sortByName
+    // Burbuja mejorada
+    public void sortByName(Person[] people) {
         for (int i = 0; i < people.length; i++) {
             boolean swapped = false;
             for (int j = 0; j < people.length - 1 - i; j++) {
-                if (people[j].getName().compareTo(people[j + 1].getName()) > 0) { //Aqui se hace el cambio 
+                if (people[j].getName().compareToIgnoreCase(people[j + 1].getName()) > 0) {
                     Person temp = people[j];
                     people[j] = people[j + 1];
                     people[j + 1] = temp;
                     swapped = true;
                 }
                 if (swapped) {
-                    i = -1; 
-                    break;  
+                    i = -1;
+                    break;
                 }
             }
         }
